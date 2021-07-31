3ff0a64a3c2e5361b9487e2caa5dd4a9cc0e84cd
Erik Olsen <erik.olsen@milespartnership.com>
2021-07-31 12:33:49 -0400

    test

---
+++ models/person.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+const partialUrl = process.env.MONGODB_LOCATION;
+console.log(`Connecting to ${partialUrl}`);
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((res) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("error connecting to MongoDB: ", err.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  created: { type: Date, default: Date.now },
+  modified: Date,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
